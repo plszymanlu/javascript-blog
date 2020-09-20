aef5bf2401cad8e01f085bd9066992d8a741be63
lukasz <plszymanlu@gmail.com>
2020-09-20 22:33:14 +0200

    ZmianaElementuNaStronie

--- script.js
+++ script.js
@@ -15,3 +15,37 @@ function titleClickHandler(){
 }
 const buttonTest = document.getElementById('button-test');
 buttonTest.addEventListener('click', titleClickHandler);
+
+const links = document.querySelectorAll('.titles a');
+
+for(let link of links){
+  console.log(link);
+}
+
+'use strict';
+function titleClickHandler(event){
+    console.log('Link was clicked!');
+
+const links = document.querySelectorAll('.titles a');
+
+for(let link of links){
+    link.addEventListener('click', titleClicHandler);
+}
+
+/* remove class 'active' from all article links  */
+
+const activeLinks = document.querySelectorAll('.titles a.active');
+
+for(let activeLink of activeLinks){
+    activeLink.classList.remove('active');
+}
+
+/* remove class 'active' from all articles */
+
+const activeArticles = document.querySelectorAll('.titles a.active');
+
+for(let activeArticle of activeArticles){
+    activeArticle.classList.remove('active');
+}
+
+}
